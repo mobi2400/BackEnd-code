b09f3b5d01d75f0fe2c3f96e4e8e5fd1a1e6cfde
mobasshir <mobasshirkhan9931@gmail.com>
2025-05-24 01:32:46 +0530

    connected database

--- Authentication/9.1+Authentication+Lv.1/index.js
+++ Authentication/9.1+Authentication+Lv.1/index.js
@@ -1,9 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "your_username",
+  host: "localhost",
+  database: "your_database",
+  password: "your_password",
+  port: 5432,
+});
+db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -17,9 +27,13 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
+  
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  console.log(req.body.username);
+  console.log(req.body.password);
+});
 
 app.post("/login", async (req, res) => {});
 
