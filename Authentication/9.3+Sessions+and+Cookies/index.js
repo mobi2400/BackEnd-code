f425d529f135f13a3e02a575a7153015fcfe9ea5
mobasshir <mobasshirkhan9931@gmail.com>
2025-05-26 22:43:04 +0530

    added cookie timePeriod

--- Authentication/9.3+Sessions+and+Cookies/index.js
+++ Authentication/9.3+Sessions+and+Cookies/index.js
@@ -19,7 +19,9 @@ app.use(session({
   secret: "TOPSECRETWORD",
   resave : false,
   saveUninitialized : true, 
-
+  cookie:{
+    maxAge: 1000*60*60*24,
+  }
 }))
 
 app.use(passport.initialize());
