45f6d15c582f01dbead705555d5f88302d93dda0
mobasshir <mobasshirkhan9931@gmail.com>
2025-06-01 00:26:10 +0530

    displayed user seccret after authenticating

--- Authentication/9.6+Secrets+Project/index.js
+++ Authentication/9.6+Secrets+Project/index.js
@@ -56,10 +56,24 @@ app.get("/logout", (req, res) => {
   });
 });
 
-app.get("/submit", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render("secrets.ejs");
+app.get("/secrets", async (req, res) => {
 
+  if (req.isAuthenticated()) {
+  try {
+    const result = await db.query("SELECT secret FROM users WHERE email = $1",[req.user.email]);
+    const secret = result.rows[0].secret;
+    if (secret) {
+      res.render("secrets.ejs",{secret:secret});
+    }
+     else{
+      res.render("secret.ejs",{secret:"please add a secret"})
+     }
+  }catch(err){
+    console.error(err);
+    
+
+  }
+   
     //TODO: Update this to pull in the user secret to render in secrets.ejs
   } else {
     res.redirect("/login");
